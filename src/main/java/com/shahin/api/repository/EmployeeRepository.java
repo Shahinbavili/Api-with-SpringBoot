8080beb58f6616522becdc2800c1d1bd4ca24f20
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2024-02-08 20:52:34 +0100

    create a rest controller

---
+++ src/main/java/com/shahin/api/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.shahin.api.repository;
+
+
+import com.shahin.api.model.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
